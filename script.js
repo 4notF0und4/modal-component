9ce53bd2d7bdc8011c2fc8b92cac7c4e2ecd02a1
Asiman Jafaraov <jafaraslan522@gmail.com>
2024-01-20 23:26:18 +0400

    fix: removed comments

--- script.js
+++ script.js
@@ -1,22 +1,3 @@
-// const arrow = (a, b) => a + b; // arrow funksyasinin sade numunesi
-// console.log(arrow(9, 5));
-// /* --------------------------------------------------- */
-
-// const numbers1 = [1, 2, 3]; // "..."-spread operatoru iki arrayi bir-biriyle birleshdirir
-// const numbers2 = [4, 5, 6];
-
-// const concatNumbers = [...numbers1, ...numbers2];
-
-// document.write(concatNumbers);
-// /* --------------------------------------------------- */
-
-// const ededler = [12, 16, 56, 63, 99]; //subtract emeliyyati  arrayin ichindeki elementlerin uzerinde riyazi emeller aparir
-// document.getElementById("demo").innerHTML = ededler.reduce(myFunc);
-
-// function myFunc(total, num) {
-//   return total + num; //burada + yazdim deye topladi - yazsaydim cixacaqdi soldan saga cavab 246 oldu
-// }
-
 const popBtn = document.getElementById("openPopUp");
 const wrapper = document.getElementById("wrapper");
 const cancelBtn = document.getElementById("cancelBtn");
